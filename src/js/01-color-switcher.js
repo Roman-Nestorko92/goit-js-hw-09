29f4d51e5908a329d9e291d623ea6eb5ccec5c99
RomanNestorko <104514561+RomanXz92@users.noreply.github.com>
2023-02-07 14:30:35 +0200

    three

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+let changes = null;
+
+btnStart.addEventListener('click', e => {
+  btnStart.setAttribute('disabled', true);
+  changes = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  btnStart.removeAttribute('disabled');
+  clearInterval(changes);
+});
+
 
